31f63ede4569e111fdb6feeb788e473ae07b75bb
Arie Allen <arie.gurneyallen@gmail.com>
2016-11-07 13:32:17 -0800

    Added owner list card with links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :description])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :description])
   end
+
 end
