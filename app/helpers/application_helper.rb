c02a732044d050ec01c377b0e103662cbee504a0
zakary <94zakary@gmail.com>
2017-01-15 18:27:13 -0800

    hide hamburger on login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def small_box_classes
     "col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-12"
   end
+
+  def hidden_ham?
+    return true if controller_name == "sessions" && action_name == "new"
+  end
 end
