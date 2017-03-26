a036e64862462689ff830dbf861bbd75fb8b88a6
zakary <94zakary@gmail.com>
2017-03-26 13:05:07 -0700

    assets magic

---
+++ public/assets/custom-081b81d3b7c7c20492317bdf10d5a799c0624094aac1b20f69feb21a5dba2c47.js
@@ -0,0 +1,1 @@
+function fadeAlert(){$(document).ready(function(){setTimeout(function(){$(".alert-success").fadeOut()},2e3)})}function hideGroupShow(e,o){console.log(o),$("."+e).hide(),$("."+o).show()}
