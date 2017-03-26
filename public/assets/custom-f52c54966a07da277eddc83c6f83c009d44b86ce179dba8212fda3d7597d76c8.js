db33caf23355873627e7eb2c34d87daa46a96987
zakary <94zakary@gmail.com>
2017-03-26 12:57:20 -0700

    precompiled assets

---
+++ public/assets/custom-f52c54966a07da277eddc83c6f83c009d44b86ce179dba8212fda3d7597d76c8.js
@@ -0,0 +1,14 @@
+function fadeAlert(){
+  $(document).ready(function(){
+    setTimeout(function(){
+    $('.alert-success').fadeOut();
+    }, 2000);
+  })
+}
+
+function hideGroupShow(hideClass, showClass){
+  console.log(showClass);
+  $("." + hideClass).hide();
+  $("." + showClass).show();
+}
+;
