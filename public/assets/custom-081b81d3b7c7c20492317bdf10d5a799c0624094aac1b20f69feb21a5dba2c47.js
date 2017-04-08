0cc6b2579d61ee5cdfb7650bc590fa776426e346
zakary <94zakary@gmail.com>
2017-04-08 12:23:36 -0700

    remove precompiled asssets

--- public/assets/custom-081b81d3b7c7c20492317bdf10d5a799c0624094aac1b20f69feb21a5dba2c47.js
+++
@@ -1,1 +0,0 @@
-function fadeAlert(){$(document).ready(function(){setTimeout(function(){$(".alert-success").fadeOut()},2e3)})}function hideGroupShow(e,o){console.log(o),$("."+e).hide(),$("."+o).show()}
